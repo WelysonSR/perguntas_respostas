db9fa8dc8135d7286dbcd10cbb7d417e2cae6ee6
Welyson Dos Santos Rosa <welysonsr@gmail.com>
2021-09-02 14:36:56 -0300

    Crinado rota e front-end form pergunta

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 //Dependências
 app.set('view engine','ejs');
 app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 //Rotas
 app.get("/",(req,res)=>{    
@@ -14,6 +16,12 @@ app.get("/perguntar",(req,res)=>{
     res.render("perguntar");
 });
 
+app.post("/salvarpergunta",(req,res)=>{ 
+    let titulo = req.body.titulo;
+    let descricao = req.body.descricao;   
+    res.send("Formulario recebido! " + titulo + " " + descricao);
+    console.log('f');
+});
 
 //Executando serve
 app.listen(8080,()=>{
